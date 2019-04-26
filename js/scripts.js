17fff20a5cb9fe4251c6db1c8e833f8e386b2dae
Denis Kibet <kibet1816@gmail.com>
2019-04-26 14:40:29 +0300

    Add the triangle pictures

--- js/scripts.js
+++ js/scripts.js
@@ -7,10 +7,19 @@ function result(){
 
    if(side1===side2&&side2===side3){
 	 answer.textContent="This is an equilateral triangle";
+	var qrEqual=document.createElement ("img")
+	qrEqual.src="pics/equilateral.png"
+	document.body.appendChild(qrEqual);
 }else if(((side1===side2&&side2!==side3)||(side1===side3&&side3!==side2)||(side2===side3&&side3!==side1))&&((side1+side2>side3)&&(side1+side3>side2)&&(side2+side3>side1))){
 	answer.textContent="This is an isosceles triangle";
+	var qrIso=document.createElement ("img")
+	qrIso.src="pics/isosceles.png"
+	document.body.appendChild(qrIso);
 }else if((side1!==side2&&side2!==side3&&side1!==side3)&&((side1+side2>side3)&&(side1+side3>side2)&&(side2+side3>side1))){
 	answer.textContent="This is a scalene triangle";
+	var qrScal=document.createElement ("img")
+	qrScal.src="pics/scalene.png"
+	document.body.appendChild(qrScal);
 }else{
 	answer.textContent="No triangle can be formed";
 }
