a0ca85794257d2999635fca2111f8640384883e4
Denis Kibet <kibet1816@gmail.com>
2019-04-26 12:19:48 +0300

    Added if statement for equilateral triangle

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,9 @@ function result(){
 // Handle user input
     var side1=document.getElementById("dimension1").value;
     var side2=document.getElementById("dimension2").value;
-    var side3=document.getElementById("dimension3").value; 
+    var side3=document.getElementById("dimension3").value;
+
+    if(side1===side2&&side2===side3){
+    	alert ("Equilateral triangle.(All sides are equal)")
+    } 
 }
