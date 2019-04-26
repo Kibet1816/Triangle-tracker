ac9386582e4a7ff9b115e176773c9d9fbf371017
Denis Kibet <kibet1816@gmail.com>
2019-04-26 13:11:07 +0300

    Added parseInt to input variables in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,17 @@
 function result(){	
 // Handle user input
-    var side1=document.getElementById("dimension1").value;
-    var side2=document.getElementById("dimension2").value;
-    var side3=document.getElementById("dimension3").value;
+    var side1=parseInt(document.getElementById("dimension1").value);
+    var side2=parseInt(document.getElementById("dimension2").value);
+    var side3=parseInt(document.getElementById("dimension3").value);
 
-    if(side1===side2&&side2===side3){
-    	alert ("Equilateral triangle.(All sides are equal)");
-    }else if((side1===side2&&side2!==side3)||(side1===side3&&side3!==side2)||(side2===side3&&side3!==side1)&&((side1+side2>side3)&&(side1+side3>side2)&&(side2+side3>side1))){
-    	alert("Isosceles triangle.(Two sides are equal)");
-    }else if(side1!==side2&&side2!==side3&&side1!==side3)&&((side1+side2>side3)&&(side1+side3>side2)&&(side2+side3>side1)){
-    	alert("Scalene triangle.(No side is equal)");
-    }
+   if(side1===side2&&side2===side3){
+	 alert("This is an equilateral triangle");
+}else if(((side1===side2&&side2!==side3)||(side1===side3&&side3!==side2)||(side2===side3&&side3!==side1))&&((side1+side2>side3)&&(side1+side3>side2)&&(side2+side3>side1))){
+	alert("This is an isosceles triangle");
+}else if((side1!==side2&&side2!==side3&&side1!==side3)&&((side1+side2>side3)&&(side1+side3>side2)&&(side2+side3>side1))){
+	alert("This is a scalene triangle");
+}else{
+	alert("No triangle can be formed");
+}
+    
 }
