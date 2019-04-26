27d678bf6a1d9b0eed1d9b0cb57ef5ed88a3d2c1
Denis Kibet <kibet1816@gmail.com>
2019-04-26 12:15:57 +0300

    Created variables in scripts.js that handle user input

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
-function result(){
-	
+function result(){	
+// Handle user input
+    var side1=document.getElementById("dimension1").value;
+    var side2=document.getElementById("dimension2").value;
+    var side3=document.getElementById("dimension3").value; 
 }
