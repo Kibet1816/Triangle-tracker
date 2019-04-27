40512484ef195de08b6da486faac3d0864945ebf
Denis Kibet <kibet1816@gmail.com>
2019-04-27 19:55:32 +0300

    Add comments in the js file

--- js/scripts.js
+++ js/scripts.js
@@ -4,22 +4,28 @@ function result(){
     var side2=parseInt(document.getElementById("dimension2").value);
     var side3=parseInt(document.getElementById("dimension3").value);
     var answer=document.getElementById("answer");
-
+// Add conditional logic for equilateral triangle;
    if(side1===side2&&side2===side3){
 	 answer.innerHTML="Equilateral triangle.(All sides are equal)";
+  // Load image for eqilateral triangle
 	var qrEqual=document.createElement ("img")
 	qrEqual.src="pics/equilateral.png"
 	document.body.appendChild(qrEqual);
+// Add conditional logic for isosceles triangle;	
 }else if(((side1===side2&&side2!==side3)||(side1===side3&&side3!==side2)||(side2===side3&&side3!==side1))&&((side1+side2>side3)&&(side1+side3>side2)&&(side2+side3>side1))){
 	answer.innerHTML="Isosceles triangle.(Only two sides are equal)";
+  // Load image for isosceles triangle
 	var qrIso=document.createElement ("img")
 	qrIso.src="pics/isosceles.png"
 	document.body.appendChild(qrIso);
+// Add conditional logic for scalene triangle;
 }else if((side1!==side2&&side2!==side3&&side1!==side3)&&((side1+side2>side3)&&(side1+side3>side2)&&(side2+side3>side1))){
 	answer.innerHTML="Scalene triangle.(Neither side is equal)";
+  // Load image for scalene triangle
 	var qrScal=document.createElement ("img")
 	qrScal.src="pics/scalene.png"
 	document.body.appendChild(qrScal);
+// Add final conditional
 }else{
 	answer.innerHTML="No triangle can be formed";
 }
